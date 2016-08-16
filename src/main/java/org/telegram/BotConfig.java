be9c65bd4a9fc99c44677c2752770ad4b85cf094
love5757 <love5757@me.com>
2016-08-16 17:53:51 +0900

    config 수정

--- src/main/java/org/telegram/BotConfig.java
+++ src/main/java/org/telegram/BotConfig.java
@@ -7,25 +7,25 @@ package org.telegram;
  * @date 20 of June of 2015
  */
 public class BotConfig {
-    public static final String WEATHER_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String WEATHER_TOKEN = "";
     public static final String WEATHER_USER = "weatherbot";
 
-    public static final String TRANSIFEX_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String TRANSIFEX_TOKEN = "";
     public static final String TRANSIFEX_USER = "TGlanguagesbot";
 
-    public static final String FILES_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String FILES_TOKEN = "";
     public static final String FILES_USER = "filesbot";
 
-    public static final String DIRECTIONS_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String DIRECTIONS_TOKEN = "";
     public static final String DIRECTIONS_USER = "directionsbot";
 
     public static final String CHANNEL_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
-    public static final String CHANNEL_USER = "channelupdatesbot";
+    public static final String CHANNEL_USER = "yerinaBot";
 
-    public static final String RAE_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String RAE_TOKEN = "";
     public static final String RAE_USER = "raebot";
 
-    public static final String WEBHOOK_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String WEBHOOK_TOKEN = "";
     public static final String WEBHOOK_USER = "webhooksamplebot";
 
     public static final String COMMANDS_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
