686bc1300606f2a10f71177b6f1fecc66002f215
love5757 <love5757@me.com>
2016-08-16 16:45:40 +0900

    Default

---
+++ src/main/java/org/telegram/BotConfig.java
@@ -0,0 +1,33 @@
+package org.telegram;
+
+/**
+ * @author Ruben Bermudez
+ * @version 1.0
+ * @brief Bots configurations
+ * @date 20 of June of 2015
+ */
+public class BotConfig {
+    public static final String WEATHER_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String WEATHER_USER = "weatherbot";
+
+    public static final String TRANSIFEX_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String TRANSIFEX_USER = "TGlanguagesbot";
+
+    public static final String FILES_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String FILES_USER = "filesbot";
+
+    public static final String DIRECTIONS_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String DIRECTIONS_USER = "directionsbot";
+
+    public static final String CHANNEL_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String CHANNEL_USER = "channelupdatesbot";
+
+    public static final String RAE_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String RAE_USER = "raebot";
+
+    public static final String WEBHOOK_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String WEBHOOK_USER = "webhooksamplebot";
+
+    public static final String COMMANDS_TOKEN = "244023234:AAEYilHRgd-1rR1bXO_ydOhjjsweSjSFI2A";
+    public static final String COMMANDS_USER = "yerinaBot";
+}
