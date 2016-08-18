143414d25336ffc397cc42042abaf36aef3f4858
love5757 <love5757@me.com>
2016-08-18 12:31:41 +0900

    spring add

---
+++ src/main/java/kr/yerina/constant/SystemConstants.java
@@ -0,0 +1,28 @@
+package kr.yerina.constant;
+
+/**
+ * The type System constants.
+ */
+public class SystemConstants {
+
+	/**
+	 * The constant SPRING_CONTEXT_LOCATIONS.
+	 */
+	public static final String[] SPRING_CONTEXT_LOCATIONS = {"/spring/applicationContext.xml"};
+
+	/**
+	 * 상용 또는 개발 서버 구분
+	 */
+	public static final String SERVER_TYPE = "server.type";
+	/**
+	 * The constant SERVER_TYPE_DEV.
+	 */
+	public static final String SERVER_TYPE_DEV = "dev";
+	/**
+	 * The constant SERVER_TYPE_OPERATION.
+	 */
+	public static final String SERVER_TYPE_OPERATION = "operation";
+
+	
+	
+}
---
+++ src/main/java/kr/yerina/context/RootContext.java
@@ -0,0 +1,19 @@
+package kr.yerina.context;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * ApplicationContext 생성
+ *
+ * @author bumsoo
+ */
+public class RootContext {
+
+	/**
+	 * The constant applicationContext.
+	 */
+	public final static ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+			"/spring/applicationContext.xml");
+
+}
