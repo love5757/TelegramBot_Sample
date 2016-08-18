01eec4234ef5d6fe0d773b60faef6164c8d3ecaf
love5757 <love5757@me.com>
2016-08-18 10:55:34 +0900

    lombok modify

--- src/main/java/org/telegram/domain/respones/SimsimiRespones.java
+++ src/main/java/org/telegram/domain/respones/SimsimiRespones.java
@@ -1,11 +1,14 @@
 package org.telegram.domain.respones;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * Created by philip on 2016-08-17.
  */
 
+@Data
 public class SimsimiRespones implements Serializable {
 
     private String response;
@@ -13,45 +16,4 @@ public class SimsimiRespones implements Serializable {
     private Integer result;
     private String msg;
 
-    public String getResponse() {
-        return response;
-    }
-
-    public void setResponse(String response) {
-        this.response = response;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getResult() {
-        return result;
-    }
-
-    public void setResult(Integer result) {
-        this.result = result;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    @Override
-    public String toString() {
-        return "SimsimiRespones{" +
-                "response='" + response + '\'' +
-                ", id=" + id +
-                ", result=" + result +
-                ", msg='" + msg + '\'' +
-                '}';
-    }
 }
